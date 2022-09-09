e08bbe155ecdd66a995aeabb217a9f3b2142d797
ryan-munger <112397901+ryan-munger@users.noreply.github.com>
2022-09-09 08:21:39 -0400

    Assignment 3

--- Assignment 3/Debug.py
+++ Assignment 3/Debug.py
@@ -1,28 +1,29 @@
 def main():
     
     #Can you print out "Hello" 8 times? I gave you a tiny hint to start...
-    for x in range(?):
+    for x in range(8):
+        print("Hello")
         
     #What about as a while loop?
-    loops = ?
-    while (loops < ?):
-        
+    loops = 0
+    while (loops < 8):
+        print("While Hello")
+        loops += 1
     
     #The loop iterations are one behind in a non-programming counting way... how can we fix this?
     count = 0
-    while (count < 3):   
-        print("While loop iteration", count)
+    while (count < 3):
         count = count + 1
-        
+        print("While loop iteration", count)
+
     #Same deal here!
     for x in range(3):
-        print("For loop iteration:", x)
+        print("For loop iteration:", x+1)
      
     #Uh oh I messed up and made an infinite loop... so silly of me!   
     endless = 4    
     while (endless < 5):
         print("I'm stuck in this loop!!!!")
-    
-    
-    
+        endless += 1
+
 main()
