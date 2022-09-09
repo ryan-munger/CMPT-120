01361df7b47d01d54f633cd21d8a3a2cf89a12dc
ryan-munger <112397901+ryan-munger@users.noreply.github.com>
2022-09-09 08:08:04 -0400

    Assignment 1

--- Assignment 1/Debug.py
+++ Assignment 1/Debug.py
@@ -1,33 +1,28 @@
 
-#Assignment 1: Debug.py
-#Fix any syntax errors you can spot and fulfill any other requests
+# Assignment 1: Debug.py
+# Fix any syntax errors you can spot and fulfill any other requests
 def main():
-    print(hello)
+    print("hello")
     print(3)
     print(10 + 10)
 
-
-    #insert your name in this string below
-    print("My name is ")
-    #this is printing a string and not an integer. how do we fix it?
+    # insert your name in this string below
+    print("My name is Ryan")
+    # this is printing a string and not an integer. how do we fix it?
     print(50)
-    #make sure this one prints out the sum
-    print("2 + 2")
+    # make sure this one prints out the sum
+    print(2 + 2)
 
-    #insert your name into this variable
-    name = ""
-    print("Hello, my name is ", name)
+    # insert your name into this variable
+    name = "Ryan"
+    print("Hello, my name is", name)
 
-    #insert your best friend's name here and have it print out
-    name2 = ""
-    print("Hello, my best friend's name is ", name)
-    
-    #put your age here
-    age = 
+    # insert your best friend's name here and have it print out
+    name2 = "Jun"
+    print("Hello, my best friend's name is", name2)
+
+    # put your age here
+    age = 17
     print("I'm " + name + " and i'm " + str(age) + " years old")
-    
- 
-   
 
 main()
-
--- Assignment 1/helloworld.py
+++ Assignment 1/helloworld.py
@@ -1,16 +1,12 @@
-#instructions: print out "Hello World!"
+#Print hello world
 #create a variable to hold your favorite color, then print out that variable with a string beforehand that says "My favorite color is: "
 #create a variable to hold your birthday (NO SLASHES, JUST THE DIGITS) and then print out "My birthday is: " with the variable
 
-
-
 def main():
+    print("Hello World!")
+    color = "Blue"
+    print("My favorite color is: " + color)
+    bday = 102904
+    print("My birthday is: " + str(bday))
 
-
-
-    
-    
-    
-    
-    
 main()
